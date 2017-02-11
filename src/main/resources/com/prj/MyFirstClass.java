bf26fd2a1e066aa7fd487e03962abe353dd97d24
Debdas <debdas.mondal@gmail.com>
2017-02-11 11:00:27 +0530

    First commit

---
+++ src/main/resources/com/prj/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.prj;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi.....");
+		System.out.println("Hi.test...1111111111113333333333333wwwwwwwww.");
+
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hellooooo World!!e</h2>
+  
+</body>
+</html>
